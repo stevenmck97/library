9ea39ba048c85e02437de55a939d3a5dc147b205
Steven McKeown <stevenmckeown19@gmail.com>
2022-03-10 19:04:53 +0000

    Created addBookToLibrary function to add book values from user input to myLibrary Array. Created createBookCard to create the html elements needed to display them. Created displayBooks to display every book object in the array. Removed form tags in html which was preventing input values being read

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Create empty array
 let myLibrary = [];
+const booksContainer = document.querySelector(".display-container");
+const addBookBtn = document.querySelector("#addBtn");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -8,10 +10,41 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBook() {
-    const testBook = new Book("test", "yo", "3", "yes");
-    myLibrary.push(testBook);
+function displayBooks() {
+    for (book in myLibrary) {
+        createBookCard(myLibrary[book]);
+    }
+}
+
+function addBookToLibrary() {
+    const bookTitle = document.querySelector("#title").value;
+    const bookAuthor = document.querySelector("#author").value;
+    const bookPages = document.querySelector("#pages").value;
+    const bookRead = document.querySelector("#read").value;
+    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+    myLibrary.push(newBook);
+    displayBooks();
     console.log(myLibrary);
 }
 
-addBook();
+function createBookCard(book) {
+    const bookCard = document.createElement("div");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("p");
+
+    bookCard.classList.add("book-card");
+
+    title.textContent = `"${book.title}"`;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+    read.textContent = book.read;
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+
+    booksContainer.appendChild(bookCard);
+}
